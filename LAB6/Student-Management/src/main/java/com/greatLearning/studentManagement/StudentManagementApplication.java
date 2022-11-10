31f1e5d7201575086632598cd6acc6f45b1dbeaf
manojgl22 <105113461+manojgl22@users.noreply.github.com>
2022-11-10 19:53:09 +0530

    Add files via upload

---
+++ LAB6/Student-Management/src/main/java/com/greatLearning/studentManagement/StudentManagementApplication.java
@@ -0,0 +1,14 @@
+//	lab 6 restfull solution for 20 marks.
+package com.greatLearning.studentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
